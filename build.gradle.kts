e506f1b59015d2990ab2719f44c1abfa5bef0a05
Pegbeer <55509957+Pegbeer@users.noreply.github.com>
2023-01-19 21:43:05 -0600

    Libraries setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,18 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import com.google.cloud.tools.gradle.appengine.appyaml.AppEngineAppYamlExtension
 
 plugins {
     id("org.springframework.boot") version "2.7.8"
     id("io.spring.dependency-management") version "1.1.0"
     kotlin("jvm") version "1.7.22"
     kotlin("plugin.spring") version "1.7.22"
+    kotlin("plugin.serialization") version "1.7.20"
+    id("com.google.cloud.tools.appengine") version "2.4.2"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 group = "com.pegbeer"
-version = "0.0.1-SNAPSHOT"
+version = "0.0.1"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
@@ -22,12 +26,32 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
+}
+
+configure<AppEngineAppYamlExtension>{
+    stage{
+        setArtifact("build/libs/${project.name}.jar")
+    }
+    deploy {
+        version = "GCLOUD_CONFIG"
+        projectId = "GCLOUD_CONFIG"
+    }
 }
 
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
         jvmTarget = "11"
+    }
+}
+
+tasks {
+    bootJar {
+        archiveFileName.set("${project.name}.jar")
+    }
+    jar{
+        archiveFileName.set("${project.name}.jar")
     }
 }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,11 @@
 rootProject.name = "payments-app-api"
+
+pluginManagement{
+    resolutionStrategy{
+        eachPlugin{
+            if (requested.id.id.startsWith("com.google.cloud.tools.appengine")) {
+                useModule("com.google.cloud.tools:appengine-gradle-plugin:${requested.version}")
+            }
+        }
+    }
+}
